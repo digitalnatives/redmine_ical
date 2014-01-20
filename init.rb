ed1ab0556aeb50d7251cd56d86df916405c87dce
zs <baloghzsof@gmail.com>
2014-01-20 01:48:28 +0100

    Initial commit.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_ical do
+  name 'Redmine Ical plugin'
+  author 'Digital Natives'
+  description 'This is a plugon for redmine that generates ical for the specified trackers.'
+  version '0.0.1'
+  url 'tba'
+  author_url 'tba'
+end
