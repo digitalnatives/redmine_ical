386a47ed0c6183cf945a2f35a03138aa44cfec26
Iuri Fernandes <iurifq@gmail.com>
2014-01-21 17:27:03 +0100

    Add javascript to sync dates between date pickers.

--- assets/javascripts/datetimepicker.js
+++ assets/javascripts/datetimepicker.js
@@ -1,4 +1,31 @@
+function syncDateInput(dateInput, dateTimeInput) {
+  dateInput.val(dateTimeInput.val().split(' ')[0]);
+  dateTimeInput.val(dateTimeInput.val().split(' ')[1]);
+}
+
+function syncDateTimeInput(dateInputSelector, $dateTimeInput){
+  $dateTimeInput.val($(dateInputSelector).val() + ' ' + $dateTimeInput.val());
+}
+
 $(function() {
-  $('#issue_starting_hours').datetimepicker();
-  $('#issue_finishing_hours').datetimepicker();
+  $('#issue_starting_hours').datetimepicker({
+    format: 'Y-m-d H:i',
+    validateOnBlur: false,
+    onChangeDateTime: function(dp, $input){
+      syncDateInput($('#issue_start_date'), $input);
+    },
+    onShow: function(dp, $input){
+      syncDateTimeInput('#issue_start_date', $input)
+    }
+  });
+  $('#issue_finishing_hours').datetimepicker({
+    format: 'Y-m-d H:i',
+    validateOnBlur: false,
+    onChangeDateTime: function(dp, $input){
+      syncDateInput($('#issue_due_date'), $input);
+    },
+    onShow: function(dp, $input){
+      syncDateTimeInput('#issue_due_date', $input)
+    }
+  });
 });
