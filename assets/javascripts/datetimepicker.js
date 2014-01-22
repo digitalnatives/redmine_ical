adbe329d872a0bccbe37ef49397b537a8f19d64c
Iuri Fernandes <iurifq@gmail.com>
2014-01-22 15:53:54 +0100

    Solve dates sync bugs.

--- assets/javascripts/datetimepicker.js
+++ assets/javascripts/datetimepicker.js
@@ -1,31 +1,47 @@
-function syncDateInput(dateInput, dateTimeInput) {
-  dateInput.val(dateTimeInput.val().split(' ')[0]);
+function syncDateInputs(dateInput, dateTimeInput) {
+  var dateTimeParts = dateTimeInput.val().split(' ');
+  if (dateTimeParts.length > 1) {
+    dateInput.val(dateTimeParts[0]);
+    dateTimeInput.val(dateTimeParts.pop());
+  }
+}
+
+function removeDateFromDateTimeInput(dateTimeInput) {
   dateTimeInput.val(dateTimeInput.val().split(' ').pop());
 }
 
-function syncDateTimeInput(dateInputSelector, $dateTimeInput){
+function syncDateTimeInput(dateInputSelector, $dateTimeInput, datetimepicker){
   $dateTimeInput.val($(dateInputSelector).val() + ' ' + $dateTimeInput.val().split(' ').pop());
+
+  var datetime_data = datetimepicker.data().xdsoft_datetime;
+  datetime_data.setCurrentTime(datetime_data.strtodatetime($dateTimeInput.val()))
 }
 
 $(function() {
   $('#issue_starting_hours').datetimepicker({
-    format: 'Y-m-d H:i',
+    format: 'Y-m-d H:i:s',
     validateOnBlur: false,
     onChangeDateTime: function(dp, $input){
-      syncDateInput($('#issue_start_date'), $input);
+      syncDateInputs($('#issue_start_date'), $input);
+    },
+    onClose: function(dp, $input){
+      removeDateFromDateTimeInput($input);
     },
     onShow: function(dp, $input){
-      syncDateTimeInput('#issue_start_date', $input)
+      syncDateTimeInput('#issue_start_date', $input, this)
     }
   });
   $('#issue_finishing_hours').datetimepicker({
-    format: 'Y-m-d H:i',
+    format: 'Y-m-d H:i:s',
     validateOnBlur: false,
     onChangeDateTime: function(dp, $input){
-      syncDateInput($('#issue_due_date'), $input);
+      syncDateInputs($('#issue_due_date'), $input);
+    },
+    onClose: function(dp, $input){
+      removeDateFromDateTimeInput($input);
     },
     onShow: function(dp, $input){
-      syncDateTimeInput('#issue_due_date', $input)
+      syncDateTimeInput('#issue_due_date', $input, this)
     }
   });
 });
