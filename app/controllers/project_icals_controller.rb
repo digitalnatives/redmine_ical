b242b3d06b85c7bb04f7281dc63899bf748d4e75
Iuri Fernandes <iurifq@gmail.com>
2014-01-28 18:00:45 +0100

    Verify token validity on ical file access.

--- app/controllers/project_icals_controller.rb
+++ app/controllers/project_icals_controller.rb
@@ -1,8 +1,19 @@
 class ProjectIcalsController < ApplicationController
   unloadable
 
+  before_filter :find_project
+  before_filter :check_token
+
+  def find_project
+    @project = Project.find_by_identifier(params[:project_id])
+  end
+
+  def check_token
+    token = IcalDownloadToken.where(project_id: @project.id, token: params[:token]).first
+    render_403 if token.expired?
+  end
+
   def show
-    project = Project.find(params[:project_id])
-    send_file project.cal_filename
+    send_file @project.cal_filename
   end
 end
