256e4403a5270aeffd9ba442164a3b8d12f36668
Iuri Fernandes <iurifq@gmail.com>
2014-01-30 11:02:15 +0100

    Fix conflict with redmine_backlogs due to bad file naming.
    Thanks @bonyiii :thumbsup:

--- lib/ical_view_hook_listener.rb
+++ lib/ical_view_hook_listener.rb
@@ -1,5 +1,5 @@
 class IcalViewHookListener < Redmine::Hook::ViewListener
   render_on :view_issues_form_details_bottom, :partial => 'issues/custom_attributes'
-  render_on :view_issues_form_details_head, :partial => 'hooks/rb_include_scripts'
+  render_on :view_issues_form_details_bottom, :partial => 'hooks/ri_include_scripts'
   render_on :view_projects_show_left, :partial => 'projects/ical_box'
 end
