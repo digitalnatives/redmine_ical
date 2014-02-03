e745fca3a4b07142bf6d0d7f7ec3eb6167be7b76
Iuri Fernandes <iurifq@gmail.com>
2014-02-03 17:00:35 +0100

    Fix conflict between view hooks.

--- lib/ical_view_hook_listener.rb
+++ lib/ical_view_hook_listener.rb
@@ -1,5 +1,5 @@
 class IcalViewHookListener < Redmine::Hook::ViewListener
-  render_on :view_issues_form_details_bottom, :partial => 'issues/custom_attributes'
-  render_on :view_issues_form_details_bottom, :partial => 'hooks/ri_include_scripts'
+  render_on :view_layouts_base_html_head, :partial => 'hooks/ri_include_scripts'
+  render_on :view_issues_form_details_bottom, :partial => 'issues/ri_custom_attributes'
   render_on :view_projects_show_left, :partial => 'projects/ical_box'
 end
